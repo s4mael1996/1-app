6be9e360554e33ab0d8e34fcaee5f4ce719d2984
s4mael1996 <nhat@Nhats-MacBook-Pro.local>
2020-01-10 23:48:04 +0700

    connectAPI

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends React.Component {
   };
   }
   getDataFromDb=()=>{
-    axios.get('http://https://nhatsfirstapp.herokuapp.com/')
+    axios.get('https://nhatsfirstapp.herokuapp.com/')
       .then(res => {
         let projects = res.data;
         
