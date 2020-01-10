57854d276667755c53d44e2bba53e009b3864a36
s4mael1996 <nhat@Nhats-MacBook-Pro.local>
2020-01-10 12:39:50 +0700

    Updates

---
+++ precache-manifest.a82b6d405a39922e09cdd2dd3dcd521b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaac9528de7b5c308d512c024aa3a9a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5dae00d09a45fc646e7b",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "2d1bcd42bcb4a421c288",
+    "url": "/static/css/main.7b5ca181.chunk.css"
+  },
+  {
+    "revision": "5dae00d09a45fc646e7b",
+    "url": "/static/js/2.50e7f565.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/static/js/2.50e7f565.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2d1bcd42bcb4a421c288",
+    "url": "/static/js/main.ac9363d6.chunk.js"
+  },
+  {
+    "revision": "16bf403f85a67a0fb936",
+    "url": "/static/js/runtime-main.12a6431d.js"
+  }
+]);
